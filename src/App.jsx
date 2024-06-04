8466aedb43786b1718f935b7d9e5c18a7dfeeaf4
unknown <kchethan@falcasolutions.com>
2024-06-04 21:50:07 +0530

    Changes in Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React from "react";
 // import './App.css'
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './pages/Layout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import ErrorPage from './ErrorPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import ErrorPage from "./ErrorPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact/>} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <div>
-      
+      <ul>
+        <li>
+          <a href="/admin/">Home</a>
+        </li>
+        <li>
+          <a href="/admin/about">About</a>
+        </li>
+        <li>
+          <a href="/admin/contact">Contact</a>
+        </li>
+      </ul>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
